77cda6de735b6b4adee4eb799d892faf4fd17506
thepasto <thepasto@hotmail.it>
2011-09-13 02:03:11 +0200

    Support for Qualcomm MSM7201/QSD8X50 on-chip EHCI USB Host controller 2 :)

--- drivers/usb/host/ehci-hcd.c
+++ drivers/usb/host/ehci-hcd.c
@@ -933,6 +933,13 @@ MODULE_LICENSE ("GPL");
 #define PLATFORM_DRIVER		ehci_msm_driver
 #endif
 
+
+#ifdef CONFIG_USB_EHCI_MSM7201
+#include "ehci-msm7201.c"
+#define PLATFORM_DRIVER   ehci_msm7201_driver
+#endif
+
+
 #if !defined(PCI_DRIVER) && !defined(PLATFORM_DRIVER) && \
     !defined(PS3_SYSTEM_BUS_DRIVER) && !defined(OF_PLATFORM_DRIVER)
 #error "missing bus glue for ehci-hcd"
@@ -966,6 +973,7 @@ static int __init ehci_hcd_init(void)
 #endif
 
 #ifdef PLATFORM_DRIVER
+pr_debug("#ifdef PLATFORM_DRIVER\n");
 	retval = platform_driver_register(&PLATFORM_DRIVER);
 	if (retval < 0)
 		goto clean0;
