4dd4f32f993a226ddcaa83d10139e08060a747c7
thepasto <thepasto@gmail.com>
2012-10-03 15:46:35 +0200

    x_tables merge fix

--- include/linux/netfilter/x_tables.h
+++ include/linux/netfilter/x_tables.h
@@ -201,11 +201,6 @@ struct xt_action_param {
 		const void *matchinfo, *targinfo;
 	};
 	const struct net_device *in, *out;
-<<<<<<< HEAD
-	//const struct xt_match *match;
-	//const void *matchinfo;
-=======
->>>>>>> approved
 	int fragoff;
 	unsigned int thoff;
 	unsigned int hooknum;
